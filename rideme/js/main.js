4dd9f4be3030b34d118206a972974c15984b2c9d
GonFreecss42 <128959039+GonFreecss42@users.noreply.github.com>
2023-04-03 18:23:17 +0200

    son

---
+++ rideme/js/main.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+ new Swiper('.swiper', {
+    loop: false, 
+    autoplay: false,
+    // width: 250,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+}
