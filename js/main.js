54ffdba717eadba6f6c06dad20145d68afc72156
Khassan Aushev <aushevhas@MacBook-Air-Khassan.local>
2023-04-06 11:00:31 +0200

    desctop done

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
 window.onload = function () {
- new Swiper('.swiper', {
-    loop: false, 
+  new Swiper('.swiper', {
+    loop: false,
     autoplay: false,
     // width: 250,
     pagination: {
       el: '.swiper-pagination',
     },
   });
+
+  const menuLinks = document.querySelectorAll(".header__menu__link, .footer__menu__link");
+  const helpbutton = document.querySelector(".header__button");
+
+  helpbutton.addEventListener("click", (event) => {
+    event.preventDefault();
+    document.getElementById("footer").scrollIntoView(
+      {
+        behavior: "smooth",
+      }
+    )
+  }
+  )
+
+  for (const link of menuLinks) {
+    // это айдишник блока
+    const sectionID = link.getAttribute("href").replace("#", "");
+    const section = document.getElementById(sectionID);
+    // а здесь клик на ссылку меню футере и в хедере
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      section.scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  }
+
 }
+
